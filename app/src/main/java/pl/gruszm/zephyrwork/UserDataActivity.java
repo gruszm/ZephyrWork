2474ce875124c22acbc75581e56540f2eb1e1cc4
gruszm <mariuszgruszczynski11@gmail.com>
2024-03-29 21:34:00 +0100

    Created view for user data

---
+++ app/src/main/java/pl/gruszm/zephyrwork/UserDataActivity.java
@@ -0,0 +1,15 @@
+package pl.gruszm.zephyrwork;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserDataActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_data);
+    }
+}
