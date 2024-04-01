aa4fd1611d2fd54171bab934e31bb3e4cde4b7e9
gruszm <mariuszgruszczynski11@gmail.com>
2024-04-01 17:03:26 +0200

    Work Session XML renamed

--- app/src/main/java/pl/gruszm/zephyrwork/WorkSessionActivity.java
+++ app/src/main/java/pl/gruszm/zephyrwork/WorkSessionActivity.java
@@ -41,7 +41,7 @@ public class WorkSessionActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.work_session);
+        setContentView(R.layout.activity_work_session);
 
         callLock = false;
 
