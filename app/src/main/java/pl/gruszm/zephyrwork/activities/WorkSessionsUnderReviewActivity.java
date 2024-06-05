07e0ffff722321a165182f4a87a0408cad26b2ac
gruszm <mariuszgruszczynski11@gmail.com>
2024-06-05 23:15:47 +0200

    Sidebar copied to work sessions under review

--- app/src/main/java/pl/gruszm/zephyrwork/activities/WorkSessionsUnderReviewActivity.java
+++ app/src/main/java/pl/gruszm/zephyrwork/activities/WorkSessionsUnderReviewActivity.java
@@ -4,8 +4,12 @@ import static pl.gruszm.zephyrwork.config.AppConfig.CONNECTION_ERROR_STANDARD_MS
 import static pl.gruszm.zephyrwork.enums.RoleType.EMPLOYEE;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,8 +18,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -36,6 +42,7 @@ import pl.gruszm.zephyrwork.adapters.WorkSessionsUnderReviewAdapter;
 import pl.gruszm.zephyrwork.config.AppConfig;
 import pl.gruszm.zephyrwork.enums.RoleType;
 import pl.gruszm.zephyrwork.enums.WorkSessionState;
+import pl.gruszm.zephyrwork.navigation.MyOnNavigationItemSelectedListener;
 
 public class WorkSessionsUnderReviewActivity extends AppCompatActivity
 {
@@ -46,18 +53,57 @@ public class WorkSessionsUnderReviewActivity extends AppCompatActivity
     private OkHttpClient okHttpClient;
     private SharedPreferences sharedPreferences;
 
+    // Layout
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
+    // Navigation Header Views
+    private TextView navFirstNameAndLastName, navEmail;
+    private String userRole;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_employees_work_sessions);
+        setContentView(R.layout.activity_work_sessions_under_review);
 
+        // Layout
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        // Configure navigation
+        setSupportActionBar(toolbar);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Navigation Header Views
+        navFirstNameAndLastName = navigationView.getHeaderView(0).findViewById(R.id.nav_header_name);
+        navEmail = navigationView.getHeaderView(0).findViewById(R.id.nav_header_email);
+
+        // Toolbar and navigation handling
         if (savedInstanceState == null)
         {
             Bundle extras = getIntent().getExtras();
 
             role = RoleType.valueOf(extras.getString("user_role", EMPLOYEE.name()));
+            userRole = extras.getString("user_role");
+            navFirstNameAndLastName.setText(extras.getString("nav_first_and_last_name", ""));
+            navEmail.setText(extras.getString("email", ""));
         }
+
+        MyOnNavigationItemSelectedListener itemSelectedListener = new MyOnNavigationItemSelectedListener(
+                this,
+                userRole,
+                navFirstNameAndLastName.getText().toString(),
+                navEmail.getText().toString(),
+                drawerLayout
+        );
+        toolbar.setNavigationOnClickListener(this::navigationOnClickListener);
+        navigationView.setNavigationItemSelectedListener(itemSelectedListener);
 
         recyclerView = findViewById(R.id.recycler_view);
         progressBar = findViewById(R.id.progress_bar);
@@ -69,6 +115,18 @@ public class WorkSessionsUnderReviewActivity extends AppCompatActivity
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         retrieveWorkSessions();
+    }
+
+    private void navigationOnClickListener(View view)
+    {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
     }
 
     private void retrieveWorkSessions()
