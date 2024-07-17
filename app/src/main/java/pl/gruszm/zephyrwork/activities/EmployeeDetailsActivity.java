4e769739a1ac2a83aa07404b2421d372448d3f97
gruszm <mariuszgruszczynski11@gmail.com>
2024-07-17 23:03:37 +0200

    Save interval icon

--- app/src/main/java/pl/gruszm/zephyrwork/activities/EmployeeDetailsActivity.java
+++ app/src/main/java/pl/gruszm/zephyrwork/activities/EmployeeDetailsActivity.java
@@ -1,10 +1,13 @@
 package pl.gruszm.zephyrwork.activities;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,7 +32,7 @@ public class EmployeeDetailsActivity extends AppCompatActivity
     private UserDTO userDTO;
     private TextView employeeIdTv, email, firstName, lastName, role;
     private EditText intervalEt;
-    private Button saveIntervalBtn;
+    private ImageButton saveIntervalBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -59,11 +62,38 @@ public class EmployeeDetailsActivity extends AppCompatActivity
             intervalEt.setText(String.valueOf(userDTO.getLocationRegistrationInterval()));
         }
 
+        saveIntervalBtn.setVisibility(View.GONE);
         saveIntervalBtn.setOnClickListener(this::saveIntervalOnClickListener);
+
+        intervalEt.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                saveIntervalBtn.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+
+            }
+        });
     }
 
     private void saveIntervalOnClickListener(View view)
     {
+        if (intervalEt.getText().toString().isEmpty())
+        {
+            return;
+        }
+
         SharedPreferences sharedPreferences = getSharedPreferences(AppConfig.SHARED_PREFERENCES_NAME, MODE_PRIVATE);
         Request request = new Request.Builder()
                 .get()
@@ -89,7 +119,11 @@ public class EmployeeDetailsActivity extends AppCompatActivity
             {
                 if (response.isSuccessful())
                 {
-                    runOnUiThread(() -> Toast.makeText(EmployeeDetailsActivity.this, "Saved", Toast.LENGTH_SHORT).show());
+                    runOnUiThread(() ->
+                    {
+                        saveIntervalBtn.setVisibility(View.GONE);
+                        Toast.makeText(EmployeeDetailsActivity.this, "Saved new value of interval.", Toast.LENGTH_SHORT).show();
+                    });
                 }
                 else
                 {
